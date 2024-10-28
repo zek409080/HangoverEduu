3b57d64232c022aaff7bacaf710e678a2e1ef6d6
zek409080 <161627797+zek409080@users.noreply.github.com>
2024-10-28 09:14:51 -0300

    fddg

--- Hangover/Assets/Script/BinaryInspector.cs
+++ Hangover/Assets/Script/BinaryInspector.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
+
 
 [CustomEditor(typeof(binaryArray))]
 public class BinaryTestInspector : Editor
--- Hangover/Assets/Script/CollisionDataInspector.cs
+++ Hangover/Assets/Script/CollisionDataInspector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 using UnityEditor;
 
 public class CollisionDataInspector : MonoBehaviour
